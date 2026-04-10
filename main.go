409fbc8b87a78f435cf168ed94bf6723df354d7b
wenxu <wenxu@wenxudeMacBook-Pro.local>
2026-04-10 15:00:11 +0800

    test

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
-func main() {
+func printRandomNumber() {
+	// Seed with current time to get different values across runs.
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	now := time.Now().Format("2006-01-02 15:04:05")
-	fmt.Printf("[%s] helloworld\n", now)
+	fmt.Printf("[%s] helloworld random: %d\n", now, r.Intn(100))
+}
+
+func main() {
+	// Continuously print a random number every second.
+	for {
+		printRandomNumber()
+		time.Sleep(time.Second)
+	}
 }
